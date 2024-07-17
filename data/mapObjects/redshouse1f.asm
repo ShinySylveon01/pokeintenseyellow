1cd95b5ef2a0f82b701ee4da90ac9a8dfeda9197
SailorMoonFan01 <shakibafzali@gmail.com>
2024-07-17 00:16:19 -0400

    Finished up with tilesets

--- data/mapObjects/redshouse1f.asm
+++ data/mapObjects/redshouse1f.asm
@@ -4,7 +4,7 @@ RedsHouse1FObject:
 	db 3 ; warps
 	warp 2, 7, 0, -1 ; exit1
 	warp 3, 7, 0, -1 ; exit2
-	warp 7, 1, 0, REDS_HOUSE_2F ; staircase
+	warp  7,  0, 0, REDS_HOUSE_2F ; staircase
 
 	db 1 ; signs
 	sign 3, 1, 2 ; TV
@@ -15,4 +15,4 @@ RedsHouse1FObject:
 	; warp-to
 	warp_to 2, 7, REDS_HOUSE_1F_WIDTH
 	warp_to 3, 7, REDS_HOUSE_1F_WIDTH
-	warp_to 7, 1, REDS_HOUSE_1F_WIDTH
+	warp_to  7,  0, REDS_HOUSE_1F_WIDTH
--- data/mapObjects/redshouse2f.asm
+++ data/mapObjects/redshouse2f.asm
@@ -2,11 +2,11 @@ RedsHouse2FObject:
 	db $0A ; border block
 
 	db 1 ; warps
-	warp 7, 1, 2, REDS_HOUSE_1F
+	warp  7,  0, 2, REDS_HOUSE_1F
 
 	db 0 ; signs
 
 	db 0 ; objects
 
 	; warp-to
-	warp_to 7, 1, REDS_HOUSE_2F_WIDTH
+	warp_to  7,  0, REDS_HOUSE_2F_WIDTH
